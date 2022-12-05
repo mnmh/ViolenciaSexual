75a6e34577d6ff64835cbc861293484d9c4f5038
juan.arroyo <jcarroyos@gmail.com>
2022-12-05 12:24:41 -0500

    instalación less para el manejo de hojas de estilo

--- src/components/Page.js
+++ src/components/Page.js
@@ -25,7 +25,7 @@ export default class App extends Component {
         this.setState({ content });
         content.forEach((bloque) => {
           if (bloque.props !== undefined) {
-            console.log(bloque.props);
+            console.log(bloque.props.children);
           }
         });
       })
