8ee417a561f5c13f9a4f1a1c965b933381fe2404
juan.arroyo <jcarroyos@gmail.com>
2022-12-19 18:21:29 -0500

    se omiten bloques vacios y se crea función switch/case para renderizar según tipo de bloque

--- src/components/organisms/Page.js
+++ src/components/organisms/Page.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import axios from 'axios';
-import parse from 'html-react-parser';
+import parse, { Element } from 'html-react-parser';
 import { Modal } from '../molecules/Modal';
 
 const url = 'https://museodememoria.gov.co/wp-json/wp/v2/pages';
 const page = '14941';
 
+const parser = (input) =>
+  parse(input, {
+    trim: true,
+    replace: (domNode) => {
+      if (
+        domNode instanceof Element &&
+        domNode.attribs.class === 'imagen-modal'
+      ) {
+        return <></>;
+      }
+    },
+  });
+
 class Page extends React.Component {
   state = {
     blocks: [],
     title: '',
-    classValue: '',
-    show: false,
+    obj: '',
   };
   componentDidMount() {
     axios.get(url + '/' + page).then((response) => {
-      const blocks = parse(response.data.content.rendered);
-      const title = parse(response.data.title.rendered);
-      this.setState({ blocks, title });
-      console.log(this.state);
+      const blocks = parser(response.data.content.rendered);
+      const title = parser(response.data.title.rendered);
+      const obj = parser(response.data.content.rendered);
+
+      this.setState({ blocks, title, obj });
+      console.log(blocks[17].props.className);
+      console.log(obj);
     });
+
     this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
   }
@@ -35,16 +51,26 @@ class Page extends React.Component {
   render() {
     return (
       <>
-        {this.state.blocks.map((block, i) =>
-          !block.type ? (
-            ''
-          ) : (
-            <div className='bloque' key={i}>
-              <code className='id'>posición:{i}</code>
-              {block}
-            </div>
-          )
-        )}
+        {this.state.blocks.map((block, i) => {
+          let type = block.type;
+          switch (type) {
+            case undefined:
+              return '';
+            case 'div':
+              return (
+                <div className='bloque' key={i}>
+                  <code className={'bloque: ' + i}>bloque:{i}</code>
+                  {block}
+                </div>
+              );
+            default:
+              return (
+                <code className={'bloque: ' + i} key={i}>
+                  bloque:{i}
+                </code>
+              );
+          }
+        })}
         <code>React Modal</code>
         <Modal show={this.state.show} handleClose={this.hideModal}>
           <p>Modal</p>
