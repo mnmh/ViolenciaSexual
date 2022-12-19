6863c0fe9a68f35a809c834ed404b7fbb6ed3cf3
juan.arroyo <jcarroyos@gmail.com>
2022-12-19 11:59:46 -0500

    se crea el componente de plantilla para separar los estilos del especial de la app nativa react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-//import Posts from './components/Posts';
 import Page from './components/Page';
-import './App.css';
+import { Modal } from './components/organisms/modal';
+import ViolenciaSexual from './components/templates/ViolenciaSexual';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+    };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+  }
+
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
   render() {
     return (
       <div className='App'>
+        <ViolenciaSexual />
         <Page />
+        <code>React Modal</code>
+        <Modal show={this.state.show} handleClose={this.hideModal}>
+          <p>Modal</p>
+        </Modal>
+        <button type='button' onClick={this.showModal}>
+          Open
+        </button>
       </div>
     );
   }
--- src/components/Page.js
+++ src/components/Page.js
@@ -10,20 +10,30 @@ class Page extends React.Component {
     blocks: [],
     title: '',
     classValue: '',
+    show: false,
   };
   componentDidMount() {
     axios.get(url + '/' + page).then((response) => {
       const blocks = parse(response.data.content.rendered);
       const title = parse(response.data.title.rendered);
       this.setState({ blocks, title });
-      console.log(title, blocks);
+      console.log('blocks: ' + blocks);
     });
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
   }
+
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
 
   render() {
     return (
       <>
-        <h1>{this.state.title}</h1>
         {this.state.blocks.map((block, i) =>
           !block.type ? (
             ''
